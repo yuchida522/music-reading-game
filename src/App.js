a1430268a2c8624efb309c725ebf89f174314b8f
Cori Lint <corilint@gmail.com>
2021-05-12 16:35:20 -0500

    added Dropdown component and called it in App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Buttons from './button'
+import Dropdown from './components/Dropdown'
 
 function App() {
   return (
     <div className="App">
+      <Dropdown />
       <Buttons />
     </div>
   );
